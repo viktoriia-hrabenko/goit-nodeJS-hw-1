68727391fb0eab4119b700991976c90c8aeb7502
Viktoriia Hrabenko <vikagrabenko666@gmail.com>
2023-03-10 11:59:07 +0100

    start

---
+++ contacts.js
@@ -0,0 +1,20 @@
+const fs = require("fs").promises;
+const path = require("path");
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+// TODO: udokumentuj każdą funkcję
+function listContacts() {
+  // ...twój kod
+}
+
+function getContactById(contactId) {
+  // ...twój kod
+}
+
+function removeContact(contactId) {
+  // ...twój kod
+}
+
+function addContact(name, email, phone) {
+  // ...twój kod
+}

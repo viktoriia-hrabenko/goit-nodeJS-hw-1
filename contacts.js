f6967f7422d5b2e008f3c4dedc78ae74bdbb7044
Viktoriia Hrabenko <vikagrabenko666@gmail.com>
2023-03-12 16:03:18 +0100

    end

--- contacts.js
+++ contacts.js
@@ -1,20 +1,61 @@
 const fs = require("fs").promises;
 const path = require("path");
+const uniqid = require("uniqid");
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 
-// TODO: udokumentuj każdą funkcję
-function listContacts() {
-  // ...twój kod
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    console.table(JSON.parse(data));
+    return JSON.parse(data);
+  } catch (error) {
+    console.error();
+  }
 }
 
-function getContactById(contactId) {
-  // ...twój kod
+async function getContactById(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const result = JSON.parse(data).find(
+      (contact) => contact.id === String(contactId)
+    );
+    console.table(result);
+    return result;
+  } catch (error) {
+    console.error();
+  }
 }
 
-function removeContact(contactId) {
-  // ...twój kod
+async function addContact(name, email, phone) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const updateData = [
+      ...JSON.parse(data),
+      { id: uniqid(), name, email, phone },
+    ];
+    fs.writeFile(contactsPath, JSON.stringify(updateData, null, 2));
+    console.table(updateData);
+  } catch (error) {
+    console.error();
+  }
 }
 
-function addContact(name, email, phone) {
-  // ...twój kod
+async function removeContact(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const result = JSON.parse(data).filter(
+      (contact) => contact.id !== String(contactId)
+    );
+    fs.writeFile(contactsPath, JSON.stringify(result, null, 2));
+    console.table(result);
+  } catch (error) {
+    console.error();
+  }
 }
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+};
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const contacts = require("./contacts");
 const { Command } = require("commander");
 const program = new Command();
+
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
@@ -11,23 +13,22 @@ program.parse(process.argv);
 
 const argv = program.opts();
 
-// TODO: refaktor
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      // ...
+      contacts.listContacts();
       break;
 
     case "get":
-      // ... id
+      contacts.getContactById(id);
       break;
 
     case "add":
-      // ... name email phone
+      contacts.addContact(name, email, phone);
       break;
 
     case "remove":
-      // ... id
+      contacts.removeContact(id);
       break;
 
     default:
